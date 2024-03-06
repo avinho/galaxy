352fe12bad8a4a6d5131c0a182c4826fc4713f97
Alvaro <alvaromarttinho@gmail.com>
2024-03-06 02:36:19 -0300

    Adding env's and fix CORS

--- src/main/java/com/galaxy/backend/controllers/SeguradoController.java
+++ src/main/java/com/galaxy/backend/controllers/SeguradoController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping( "api/segurados")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class SeguradoController {
 
     private final SeguradoService seguradoService;
